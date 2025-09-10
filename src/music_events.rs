152925a184ceaf5e46bf6bfc06c18e46b77a23af
MCOfficer <mcofficer@gmx.de>
2025-09-10 16:06:25 +0200

    delete obsolete hook

--- src/music_events.rs
+++ src/music_events.rs
@@ -31,35 +31,3 @@ pub async fn ready_event(client: LavalinkClient, session_id: String, event: &eve
     client.delete_all_player_contexts().await.unwrap();
     info!("{:?} -> {:?}", session_id, event);
 }
-
-#[hook]
-pub async fn track_start(client: LavalinkClient, _session_id: String, event: &events::TrackStart) {
-    let player_context = client.get_player_context(event.guild_id).unwrap();
-    let data = player_context
-        .data::<(ChannelId, std::sync::Arc<Http>)>()
-        .unwrap();
-    let (_channel_id, _http) = (&data.0, &data.1);
-
-    let msg = {
-        let track = &event.track;
-
-        if let Some(uri) = &track.info.uri {
-            format!(
-                "Now playing: [{} - {}](<{}>) | Requested by <@!{}>",
-                track.info.author,
-                track.info.title,
-                uri,
-                track.user_data.clone().unwrap()["requester_id"]
-            )
-        } else {
-            format!(
-                "Now playing: {} - {} | Requested by <@!{}>",
-                track.info.author,
-                track.info.title,
-                track.user_data.clone().unwrap()["requester_id"]
-            )
-        }
-    };
-
-    info!(msg);
-}
